4db34614eae8861948d88d376c957025d34e087d
przemyslvw <przemyslvw@gmial.com>
2024-09-21 10:40:06 +0200

    simulate heartbeat

--- main/led.cpp
+++ main/led.cpp
@@ -2,10 +2,19 @@
 #include "led.h"
 
 void blinkLED() {
-  digitalWrite(LED_BUILTIN, LOW);  // Włącz diodę
-  digitalWrite(LED_GPIO4, HIGH);  // Włącz diodę
-  delay(100);                      // Opóźnienie 100 ms
-  digitalWrite(LED_BUILTIN, HIGH); // Wyłącz diodę
-  digitalWrite(LED_GPIO4, LOW); // Wyłącz diodę
-  delay(100);  
+  // digitalWrite(LED_BUILTIN, LOW);  // Włącz diodę
+  // digitalWrite(LED_GPIO4, HIGH);  // Włącz diodę
+  // delay(100);                      // Opóźnienie 100 ms
+  // digitalWrite(LED_BUILTIN, HIGH); // Wyłącz diodę
+  // digitalWrite(LED_GPIO4, LOW); // Wyłącz diodę
+  // delay(100);
+  // heartbeat
+  digitalWrite(LED_GPIO4, HIGH); // Włącz LED
+  delay(100); // Krótkie włączenie (100 ms)
+  digitalWrite(LED_GPIO4, LOW); // Wyłącz LED
+  delay(100); // Krótka przerwa (100 ms)
+  digitalWrite(LED_GPIO4, HIGH); // Włącz LED
+  delay(300); // Dłuższe włączenie (300 ms)
+  digitalWrite(LED_GPIO4, LOW); // Wyłącz LED
+  delay(1000); // Dłuższa przerwa (1000 ms)
 }
